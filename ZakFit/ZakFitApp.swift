cdb6a94a8d60ad3733ae296130f1d4292d191527
Sébastien <sebbody79400@gmail.com>
2025-11-28 16:11:48 +0100

    Initial commit

---
+++ ZakFit/ZakFitApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZakFitApp.swift
+//  ZakFit
+//
+//  Created by Sebastien Besse on 28/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct ZakFitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
